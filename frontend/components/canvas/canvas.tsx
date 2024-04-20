4c6d2c5f00d3067bd4b0c591fbdbebaaa2205a08
Håvard Høiby <havard.hoiby@autodesk.com>
2024-04-21 01:04:50 +0200

    fix: display of models

--- frontend/components/canvas/canvas.tsx
+++ frontend/components/canvas/canvas.tsx
@@ -32,13 +32,8 @@ import {
 import Port from "../viewer/port";
 export default function Canvas() {
   return (
-    <main className="grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-3">
-      <div
-        className="relative hidden flex-col items-start gap-8 md:flex"
-        x-chunk="dashboard-03-chunk-0"
-      >
-        <Port />
-      </div>
+    <main>
+      <Port />
     </main>
   );
 }
