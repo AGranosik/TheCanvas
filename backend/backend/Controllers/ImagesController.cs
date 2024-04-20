b2b9cf6d236697b846520924db9f406c222c8f03
AGranosik <adrian.granosik@gmail.com>
2024-04-20 19:14:42 +0200

    post image

---
+++ backend/backend/Controllers/ImagesController.cs
@@ -0,0 +1,31 @@
+﻿using backend.Logic.Coomands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ImagesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Test()
+            => Ok();
+
+        [HttpPost]
+        public async Task<IActionResult> PostImage(IFormFile file, CancellationToken cancellationToken)
+        {
+            await _mediator.Send(new Save2DProjectionCommand
+            {
+                Image = file
+            }, cancellationToken);
+            return Ok();
+        }
+    }
+}
---
+++ backend/backend/Logic/Coomands/Save2DProjectionCommand.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+
+namespace backend.Logic.Coomands
+{
+    public class Save2DProjectionCommand : IRequest
+    {
+        public IFormFile Image { get; set; }
+    }
+
+    public class Save2DProjectionCommandHandler : IRequestHandler<Save2DProjectionCommand>
+    {
+        public async Task Handle(Save2DProjectionCommand request, CancellationToken cancellationToken)
+        {
+            if (request == null || request.Image == null || request.Image.Length == 0)
+                throw new ArgumentException("Sth wrong with image.");
+
+            var directoryPath = AppDomain.CurrentDomain.BaseDirectory;
+            var imagesPath = Path.Combine(directoryPath, "Images");
+            Directory.CreateDirectory(imagesPath);
+
+            var filePath = Path.Combine(imagesPath.ToString(), request.Image.FileName);
+            using var stream = new FileStream(filePath, FileMode.Create);
+            await request.Image.CopyToAsync(stream);
+        }
+    }
+}
